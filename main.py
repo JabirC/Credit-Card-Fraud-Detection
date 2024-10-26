c094bb32663a96cf7a12df65164114ef6f122160
Jabir Chowdhury <jabirchowdhury21@gmail.com>
2024-10-26 02:54:50 -0400

    Add python notebook

--- main.py
+++ main.py
@@ -82,5 +82,5 @@ def predict():
         "Probability" : probability.tolist()
     }
 
-if __name__ == '__main__':
-    app.run(debug = False, host='0.0.0.0', port=80)
+# if __name__ == '__main__':
+#     app.run(debug = False, host='0.0.0.0', port=80)
